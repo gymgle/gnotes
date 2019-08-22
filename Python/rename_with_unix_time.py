82934e9a32bc5e0fcfe4433017ea9ba6aa5f0670
Yaming Gong <ymgongcn@gmail.com>
2019-08-22 14:42:24 +0800

    Update weixin camera export python script

--- Python/rename_with_unix_time.py
+++ Python/rename_with_unix_time.py
@@ -2,14 +2,16 @@
 
 """
 Rename files with the Unix time.
-The files are come from Android Wechat folder:
+The files are in Wechat folder of Android OS:
     /Tencent/MicroMsg/WeiXin/
 rules:
     1482227010527.mp4 -> 2016-12-20 17.43.30.mp4
     mmexport1482227010527.jpg -> 2016-12-20 17.43.30.jpg
+    wx_camera_1482227010527.mp4 -> 2016-12-20 17.43.30.mp4
 
 author: Gymgle
-date: 2016-12-20
+created at: 2016-12-20
+updated at: 2019-08-22
 """
 
 import os
@@ -19,20 +21,24 @@ if __name__ == "__main__":
     PATH = "."
     LENMP4 = len("1482227010527.mp4")
     LENJPG = len("mmexport1482227010527.jpg")
+    LENWXCAM = len("wx_camera_1482227010527.mp4")
 
     for filename in os.listdir(PATH):
-        if len(filename) == LENMP4 or len(filename) == LENJPG:
-            if filename.startswith("mmexport"):
-                timestamp = filename[len("mmexport"):-4]
-                newName = datetime.datetime.fromtimestamp(int(timestamp)/1e3).strftime("%Y-%m-%d %H.%M.%S")
-                newName += ".jpg"
-            else:
-                timestamp = filename.split(".")[0]
-                newName = datetime.datetime.fromtimestamp(int(timestamp)/1e3).strftime("%Y-%m-%d %H.%M.%S")
-                newName += ".mp4"
+        if len(filename) == LENMP4:
+            timestamp = filename.split(".")[0]
+        elif len(filename) == LENJPG and filename.startswith("mmexport"):
+            timestamp = filename[len("mmexport"):-4]
+        elif len(filename) == LENWXCAM and filename.startswith("wx_camera_"):
+            timestamp = filename[len("wx_camera_"):-4] 
+        else:
+            continue
 
-            try:
-                os.rename(filename, newName)
-                print(filename + " -> " + newName)
-            except OSError as err:
-                print("OS Error: {0}{1} already exists.".format(err, newName))
+        ext = filename.split(".")[-1]
+        newName = datetime.datetime.fromtimestamp(int(timestamp)/1e3).strftime("%Y-%m-%d %H.%M.%S")
+        newName = newName + "." + ext
+
+        try:
+            os.rename(filename, newName)
+            print(filename + " -> " + newName)
+        except OSError as err:
+            print("OS Error: {0}{1} already exists.".format(err, newName))
